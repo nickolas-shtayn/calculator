0d5bc3987427c1bff65f755efeffde1a4c331d35
Nickolas Shtayn <nickolasshtayn@protonmail.com>
2024-11-19 15:48:06 -0600

    Add keyboard support for deletion

--- script.js
+++ script.js
@@ -15,19 +15,29 @@ numPad.addEventListener("click", (e) => {
     display.textContent = `${userChoices.firstNumber} ${userChoices.operation} ${userChoices.secondNumber}`;
 })
 
-document.addEventListener("keydown", (e) => {
-    let target;
+const calculator = document.querySelector("body");
 
-    if (!isNaN(e.key) && e.key !== ' ') {
-        target = e.key;
-    } else {
-        return
-    }
-
-    if (userChoices.operation == ''){
-        userChoices.firstNumber += target
-    } else {
-        userChoices.secondNumber += target
+calculator.addEventListener("keydown", (e) => {
+    let target = e.key;
+    switch (target) {
+        case 'Backspace':
+            if (userChoices.operation === ''){
+                userChoices.firstNumber = userChoices.firstNumber.slice(0,-1);
+            } else if (userChoices.secondNumber === ''){
+                userChoices.operation = userChoices.operation.slice(0,-1);
+            } else {
+                userChoices.secondNumber = userChoices.secondNumber.slice(0,-1)
+            }
+            break;
+        default:
+            if (isNaN(target)) {
+                return
+            } else if (userChoices.operation == ''){
+                userChoices.firstNumber += target
+            } else {
+                userChoices.secondNumber += target
+            }
+            break;
     }
     display.textContent = `${userChoices.firstNumber} ${userChoices.operation} ${userChoices.secondNumber}`;
 })
