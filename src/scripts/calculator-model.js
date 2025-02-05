3ce8b106abd33ed7d48139e08567848482dfa6f4
Nickolas Shtayn <nickolasshtayn@protonmail.com>
2025-02-05 17:15:00 -0600

    Remove console.logs and add exports

--- src/scripts/calculator-model.js
+++ src/scripts/calculator-model.js
@@ -4,14 +4,12 @@ let operation = '';
 let result = '';
 
 
-const getFirstNumber = () => firstNumber;
-const getSecondNumber = () => secondNumber;
-const getOperation = () => operation;
-const getResult = () => result;
+export const getFirstNumber = () => firstNumber;
+export const getSecondNumber = () => secondNumber;
+export const getOperation = () => operation;
+export const getResult = () => result;
 
-console.log("first:", getFirstNumber(), "second:", getSecondNumber(), "operation:", getOperation(), "result:", getResult());
-
-const appendToFirstNumber = (value) => {
+export const appendToFirstNumber = (value) => {
   if (value === '.' && firstNumber.includes('.')) return;
   if (value === '.' && firstNumber === '') {
     firstNumber = '0.';
@@ -20,12 +18,7 @@ const appendToFirstNumber = (value) => {
   firstNumber += value;
 };
 
-// REMOVE
-console.log("Before appending:", getFirstNumber());
-appendToFirstNumber(2);
-console.log("After appending:", getFirstNumber());
-
-const appendToSecondNumber = (value) => {
+export const appendToSecondNumber = (value) => {
   if (value === '.' && secondNumber.includes('.')) return;
   if (value === '.' && secondNumber === '') {
     secondNumber = '0.';
@@ -34,22 +27,11 @@ const appendToSecondNumber = (value) => {
   secondNumber += value;
 };
 
-// REMOVE
-console.log("Before appending:", getSecondNumber());
-appendToSecondNumber(3);
-console.log("After appending:", getSecondNumber());
-
-
-const setOperation = (value) => {
+export const setOperation = (value) => {
   operation = value;
 };
 
-// REMOVE
-console.log("Before setting:", getOperation());
-setOperation("+");
-console.log("After setting:", getOperation());
-
-const calculate = () => {
+export const calculate = () => {
   const num1 = Number(firstNumber);
   const num2 = Number(secondNumber);
 
@@ -75,29 +57,14 @@ const calculate = () => {
   }
 };
 
-// REMOVE
-firstNumber = '10';
-secondNumber = '5';
-console.log("Testing calculate() with 10 and 5:");
-['+', '-', '*', '/'].forEach(op => {
-  operation = op;
-  calculate();
-  console.log(op + ":", getResult());
-});
-
-const clear = () => {
+export const clear = () => {
   firstNumber = '';
   secondNumber = '';
   operation = '';
   result = ''; 
 };
 
-//REMOVE
-console.log("before clearing:", firstNumber, secondNumber, operation, result);
-clear();
-console.log("after clearing:", firstNumber, secondNumber, operation, result);
-
-const backspace = () => {
+export const backspace = () => {
   if (secondNumber !== '') {
     secondNumber = secondNumber.slice(0, -1);
   } else if (operation !== '') {
@@ -106,20 +73,3 @@ const backspace = () => {
     firstNumber = firstNumber.slice(0, -1);
   }
 };
-
-// REMOVE
-// Test backspace in different states
-firstNumber = '123';
-secondNumber = '456';
-operation = '+';
-
-console.log("Initial state:", firstNumber, operation, secondNumber);
-backspace(); 
-console.log("After first backspace:", firstNumber, operation, secondNumber);
-backspace();
-backspace(); 
-console.log("After clearing secondNumber:", firstNumber, operation, secondNumber);
-backspace();
-console.log("After clearing operation:", firstNumber, operation, secondNumber);
-backspace();
-console.log("After first number backspace:", firstNumber, operation, secondNumber);
