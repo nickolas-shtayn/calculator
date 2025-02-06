1092872d1709b4bac1d171b38c561a5d4a634e7b
Nickolas Shtayn <nickolasshtayn@protonmail.com>
2025-02-05 21:07:12 -0600

    Create calculator initializer function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,19 @@
-import { getFirstNumber, getSecondNumber, getOperation, getResult, 
-    appendToFirstNumber, appendToSecondNumber} from "./calculator-model";
+import { getFirstNumber, getSecondNumber, setOperation, getResult, 
+    appendToFirstNumber, appendToSecondNumber,
+    getOperation} from "./calculator-model";
 import { updateDisplay, animateButton } from "./calculator-view";
 import { setupHistory } from "./calculator-history";
 import { handleCalculatorInput } from "./calculator-controller";
 import "../styles/style.css"
 
-// import { CalculatorModel } from "./calculator-model";
-// import { CalculatorView } from "./calculator-view";
-// import { CalculatorController } from "./calculator-controller";
-// import { CalculatorHistory } from "./calculator-history";
-// import "../styles/style.css"
+const initializeCalculator = () => {
+    setupHistory((calculation) => {
+        appendToFirstNumber(calculation.firstNumber);
+        setOperation(calculation.operation);
+        appendToSecondNumber(calculation.secondNumber);
+        updateDisplay(getFirstNumber(), getSecondNumber(), getOperation(), getResult());
+    });
+};
 
 // const model = new CalculatorModel;
 // const view = new CalculatorView;
