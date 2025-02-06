14fa632cb8345897338649dd170fcdd7e9fd7cd6
Nickolas Shtayn <nickolasshtayn@protonmail.com>
2025-02-05 20:47:02 -0600

    Refactor constants in controller

--- src/scripts/calculator-controller.js
+++ src/scripts/calculator-controller.js
@@ -1,115 +1,118 @@
-export class CalculatorController {
-  constructor (model, view, history) {
-    this.model = model;
-    this.view = view;
-    this.history = history
+const OPERATIONS = ['+', '-', 'x', '*', '/', '%'];
+const SPECIAL_KEYS = ['C', "backspace", '=', "enter", '.'];
 
-    this.history.onHistorySelect = (calculation) => {
-      this.model.appendToFirstNumber(calculation.firstNumber);
-      this.model.setOperation(calculation.operation);
-      this.model.appendToSecondNumber(calculation.secondNumber);
-      this.updateView();
-    };
-  }
+// export class CalculatorController {
+//   constructor (model, view, history) {
+//     this.model = model;
+//     this.view = view;
+//     this.history = history
 
-  #OPERATIONS = ['+', '-', 'x', '*', '/', '%'];
-  #SPECIAL_KEYS = ['C', "backspace", '=', "enter", '.'];
+//     this.history.onHistorySelect = (calculation) => {
+//       this.model.appendToFirstNumber(calculation.firstNumber);
+//       this.model.setOperation(calculation.operation);
+//       this.model.appendToSecondNumber(calculation.secondNumber);
+//       this.updateView();
+//     };
+//   }
 
-  handleClick(event) {
-    const target = event.target.textContent.trim();
-    this.view.animateButton(target);
-    this.processInput(target);
-  }
+//   #OPERATIONS = ['+', '-', 'x', '*', '/', '%'];
+//   #SPECIAL_KEYS = ['C', "backspace", '=', "enter", '.'];
 
-  handleKeyPress(event) {
-    const target = event.key.toLowerCase();
-    this.view.animateButton(target);
-    this.processInput(target);
-  }
+//   handleClick(event) {
+//     const target = event.target.textContent.trim();
+//     this.view.animateButton(target);
+//     this.processInput(target);
+//   }
 
-  processInput(value) {
-    if (this.model.result) {
-      this.handlePostCalculation(value) 
-    } else {
-      this.handleNormalInput(value)
-    }
-    this.updateView();
-  }
+//   handleKeyPress(event) {
+//     const target = event.key.toLowerCase();
+//     this.view.animateButton(target);
+//     this.processInput(target);
+//   }
 
-  handlePostCalculation(value) {
-    const previousResult = this.model.result;
-    this.model.clear()
+//   processInput(value) {
+//     if (this.model.result) {
+//       this.handlePostCalculation(value) 
+//     } else {
+//       this.handleNormalInput(value)
+//     }
+//     this.updateView();
+//   }
 
-    if (!isNaN(value)) {
-      this.model.appendToFirstNumber(value);
-    } else if (this.#OPERATIONS.includes(value)) {
-      this.model.appendToFirstNumber(previousResult);
-      this.model.setOperation(value);
-    }
-  }
+//   handlePostCalculation(value) {
+//     const previousResult = this.model.result;
+//     this.model.clear()
 
-  handleNormalInput(value) {
-      // numbers
-  if (!isNaN(value)) {
-    if (this.model.operation === '') {
-      this.model.appendToFirstNumber(value);
-    } else {
-      this.model.appendToSecondNumber(value);
-      }
-  }
-  // operation
-  else if (this.#OPERATIONS.includes(value)) {
-    if (this.model.firstNumber !== '') {
-      this.model.setOperation(value);
-    }
-  }
-  // special
-  else if (this.#SPECIAL_KEYS.includes(value)) {
-    switch(value) {
-      case 'C':
-          this.model.clear();
-          break;
-      case "backspace":
-          this.model.delete();
-          break;
-      case "enter":
-      case '=':
-        if (this.model.firstNumber !== '' && this.model.operation !== '' && this.model.secondNumber !== '') {
-          this.model.calculate();
-          this.history.addToHistory({
-            firstNumber: this.model.firstNumber,
-            operation: this.model.operation,
-            secondNumber: this.model.secondNumber,
-            result: this.model.result
-          });
-        }
-          break;
-      case '.':
-          if (this.model.operation === '' && this.model.firstNumber !== '' && !this.model.firstNumber.includes(".")) {
-            this.model.appendToFirstNumber('.');
-          } else if (this.model.operation !== '' && this.model.secondNumber !== '' && !this.model.secondNumber.includes(".")) {
-            this.model.appendToSecondNumber('.');
-          }
-          break;
-    }
-  }
-  else {
-    if (value === "shift" || value === "meta") {
-      return;
-    }
-    alert(`Invalid input: ${value}`);
-    return;
-    }
-  }
+//     if (!isNaN(value)) {
+//       this.model.appendToFirstNumber(value);
+//     } else if (this.#OPERATIONS.includes(value)) {
+//       this.model.appendToFirstNumber(previousResult);
+//       this.model.setOperation(value);
+//     }
+//   }
 
-  updateView() {
-    const currentState = {
-      firstNumber: this.model.firstNumber,
-      secondNumber: this.model.secondNumber,
-      operation: this.model.operation,
-      result: this.model.result
-    };
-    this.view.updateDisplay(currentState);
-  }
+//   handleNormalInput(value) {
+//       // numbers
+//   if (!isNaN(value)) {
+//     if (this.model.operation === '') {
+//       this.model.appendToFirstNumber(value);
+//     } else {
+//       this.model.appendToSecondNumber(value);
+//       }
+//   }
+//   // operation
+//   else if (this.#OPERATIONS.includes(value)) {
+//     if (this.model.firstNumber !== '') {
+//       this.model.setOperation(value);
+//     }
+//   }
+//   // special
+//   else if (this.#SPECIAL_KEYS.includes(value)) {
+//     switch(value) {
+//       case 'C':
+//           this.model.clear();
+//           break;
+//       case "backspace":
+//           this.model.delete();
+//           break;
+//       case "enter":
+//       case '=':
+//         if (this.model.firstNumber !== '' && this.model.operation !== '' && this.model.secondNumber !== '') {
+//           this.model.calculate();
+//           this.history.addToHistory({
+//             firstNumber: this.model.firstNumber,
+//             operation: this.model.operation,
+//             secondNumber: this.model.secondNumber,
+//             result: this.model.result
+//           });
+//         }
+//           break;
+//       case '.':
+//           if (this.model.operation === '' && this.model.firstNumber !== '' && !this.model.firstNumber.includes(".")) {
+//             this.model.appendToFirstNumber('.');
+//           } else if (this.model.operation !== '' && this.model.secondNumber !== '' && !this.model.secondNumber.includes(".")) {
+//             this.model.appendToSecondNumber('.');
+//           }
+//           break;
+//     }
+//   }
+//   else {
+//     if (value === "shift" || value === "meta") {
+//       return;
+//     }
+//     alert(`Invalid input: ${value}`);
+//     return;
+//     }
+//   }
+
+//   updateView() {
+//     const currentState = {
+//       firstNumber: this.model.firstNumber,
+//       secondNumber: this.model.secondNumber,
+//       operation: this.model.operation,
+//       result: this.model.result
+//     };
+//     this.view.updateDisplay(currentState);
+//   }
   
-};
+// };
