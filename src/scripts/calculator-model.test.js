66e0c44fbc0102d5d3f105b48db8b502506b6805
Nickolas Shtayn <nickolasshtayn@protonmail.com>
2025-02-06 12:41:12 -0600

    Add test blocks for each scenario

--- src/scripts/calculator-model.test.js
+++ src/scripts/calculator-model.test.js
@@ -1,2 +1,31 @@
 import { calculate } from "./calculator-model";
 
+describe("Calculate", () => {
+    test("addition", () => {
+
+    });
+
+    test("subtraction", () => {
+        
+    });
+
+    test("multiplication", () => {
+        
+    });
+
+    test("division", () => {
+        
+    });
+
+    test("division by 0", () => {
+        
+    });
+
+    test("modulo", () => {
+        
+    });
+
+    test("modulo of 0", () => {
+        
+    });
+});
