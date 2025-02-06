bfe55199049b5a1eebddb3ae33017325f685e143
Nickolas Shtayn <nickolasshtayn@protonmail.com>
2025-02-06 12:46:22 -0600

    Add further set up for test blocks

--- src/scripts/calculator-model.test.js
+++ src/scripts/calculator-model.test.js
@@ -2,30 +2,72 @@ import { calculate } from "./calculator-model";
 
 describe("Calculate", () => {
     test("addition", () => {
+        firstNumber = ;
+        secondNumber = ;
+        operation = ;
 
+        const result = calculate();
+
+        expect(result).toBe()
     });
 
     test("subtraction", () => {
-        
+        firstNumber = ;
+        secondNumber = ;
+        operation = ;
+
+        const result = calculate();
+
+        expect(result).toBe() 
     });
 
     test("multiplication", () => {
-        
+        firstNumber = ;
+        secondNumber = ;
+        operation = ;
+
+        const result = calculate();
+
+        expect(result).toBe()        
     });
 
     test("division", () => {
-        
+        firstNumber = ;
+        secondNumber = ;
+        operation = ;
+
+        const result = calculate();
+
+        expect(result).toBe()        
     });
 
     test("division by 0", () => {
-        
+        firstNumber = ;
+        secondNumber = ;
+        operation = ;
+
+        const result = calculate();
+
+        expect(result).toBe()        
     });
 
     test("modulo", () => {
-        
+        firstNumber = ;
+        secondNumber = ;
+        operation = ;
+
+        const result = calculate();
+
+        expect(result).toBe()        
     });
 
     test("modulo of 0", () => {
-        
+        firstNumber = ;
+        secondNumber = ;
+        operation = ;
+
+        const result = calculate();
+
+        expect(result).toBe()        
     });
 });
